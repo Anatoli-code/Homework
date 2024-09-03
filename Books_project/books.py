584deadda393bfa235186324afeaa62f3ad59df3
Anatoli Yemelyanovich <emelen00759@gmail.com>
2024-09-03 16:05:37 +0300

    Correction files

--- Books_project/books.py
+++ Books_project/books.py
@@ -1,77 +1,49 @@
 import datetime
-import sys
 
-def creation():
-    dict_books = []
-    name = input("Название книги: ")
-    author = input("Название автора: ")
-    code = input("Уникальный код: ")
-    for dicts in name, author, code:
-        new_dict = {
-            "Books": name,
-            "name": author,
-            'unic_code': code
-            }
-        dict_books.append(new_dict)
-        return dict_books
+BOOK_DB = {}
+RENTED_DB = {}
+READER_DB = {}
+AVAILABLE_BOOKS_DB = {}
+DEALS_DB = {}
 
-def user_reader():
-    list_1 = []
-    ids = input("Ваше id: ")
-    NAME = input("Ваше имя: ")
-    USER_NAME = input("Ваша фамилия: ")
-    for item in ids, NAME, USER_NAME:
-        new_dict = {
-            'id': ids,
-            'name': NAME,
-            'user_name': USER_NAME
-        }
-        list_1.append(new_dict)
-        return list_1
+def save_book(book):
+    if book["code"] in BOOK_DB:
+        print(f"Книга с кодом {book['code']} уже существует.")
+    else:
+        BOOK_DB[book["code"]] = book
+        AVAILABLE_BOOKS_DB[book["code"]] = book
 
+def create_book():
+    title = input("Введите название книги: ")
+    author = input("Введите автора книги: ")
+    book_code = input("Введите уникальный код книги: ")
+    book = {"author": author, "code": book_code, "title": title}
+    save_book(book)
 
-def message():
-    sms = """
-Чтобы пройти регистрацию, вам нужно указать Имя и Фамилию, 
-а так же id на ваш выбор, чтобы продолжить нажмите Enter """
-    operation = input(sms)
-    return operation
-
-def choice_books():
-    sms = """
-Выберите книгу которая есть в наличии,
-чтобы посмотреть список доступных книг, нажмите Enter"""
-    operation = input(sms)
-    return operation
-
+def register_reader():
+    reader_id = input("Введите Ваше ID: ")
+    first_name = input("Введите Ваше имя: ")
+    last_name = input("Введите Вашу фамилию: ")
+    reader = {"id": reader_id, "first_name": first_name, "last_name": last_name}
+    READER_DB[reader_id] = reader
 
 def book_issue():
-    book_list = creation()
-    reader_list = user_reader()
-
     book_code = input("Введите код книги: ")
     reader_id = input("Введите ID читателя: ")
 
-    date = datetime.datetime.now()
-    sms_1 = f"Время фиксации {date} "
-    print("Дата выдачи зафиксирована")
-    sms_2 = f"Время фиксации {date}"
-
-    book_info = next((book for book in book_list if book['unic_code'] == book_code), None)
-    reader_info = next((reader for reader in reader_list if reader['id'] == reader_id), None)
-    if book_info and reader_info:
-        all_info = {
-            'book_title': book_info['Books'],
-            'book_author': book_info['name'],
-            'reader_name': reader_info['name'],
-            'reader_surname': reader_info['user_name'],
-            'issue_date': sms_1,
-            'return_date': sms_2
+    if book_code in AVAILABLE_BOOKS_DB and reader_id in READER_DB:
+        book = AVAILABLE_BOOKS_DB.pop(book_code)
+        RENTED_DB[book_code] = book
+        DEALS_DB[f"{reader_id}_{book_code}"] = {
+            "book_title": book["title"],
+            "book_author": book["author"],
+            "reader_id": reader_id,
+            "issue_date": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "return_date": None
         }
-        return all_info
+        print("Книга успешно выдана.")
     else:
         print("Книга или читатель не найдены.")
-        return None
 
 while True:
     print("Выберите действие:")
@@ -83,13 +55,13 @@ while True:
     choice = input("Введите номер действия: ")
 
     if choice == "1":
-        creation()
+        create_book()
     elif choice == "2":
-        user_reader()
+        register_reader()
     elif choice == "3":
         book_issue()
     elif choice == "4":
         print("Выход из программы...")
-        sys.exit()
+        break
     else:
         print("Неверный выбор. Попробуйте еще раз.")
